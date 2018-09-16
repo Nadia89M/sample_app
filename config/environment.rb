1fffec57b8ae302acd607c09ebc3b47caedc28e2
Nadia <nadia.mohamed89@gmail.com>
2018-09-16 18:29:31 +0100

    5.3: Sending Emails (1)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['app106477359@heroku.com'],
+  password: ENV['uyfeizqf9229'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
