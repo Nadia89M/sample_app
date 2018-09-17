60793df01b5167f5c3882389fe62dccb8721df72
Nadia <nadia.mohamed89@gmail.com>
2018-09-17 10:30:36 +0100

    Revert

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/landing_page'
   root 'simple_pages#index'
-  post 'simple_pages/thank_you'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
