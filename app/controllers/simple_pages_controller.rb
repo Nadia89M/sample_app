99ecedaffe700f800c12979142b7636533aeaa14
Nadia <nadia.mohamed89@gmail.com>
2018-09-18 11:21:13 +0100

    Email address changed

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,13 +3,13 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
-  
+
   def thank_you
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
     ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
+      to: 'nadia.mohamed89@gmail.com',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
     end
