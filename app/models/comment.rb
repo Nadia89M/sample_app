26c69adc7f1c40a9f3ba02c47255b26a8b389571
Nadia <nadia.mohamed89@gmail.com>
2018-10-29 15:26:46 +0000

    ActiveJob and Advanced ActionCable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  
+
   belongs_to :user
   belongs_to :product
 
@@ -10,4 +10,6 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+  
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 end
